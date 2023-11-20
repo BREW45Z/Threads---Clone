33e2412a8ef7dd9ae0acce7ce021b574d2f86021
brewz45 <michaelsadewale@protonmail.com>
2023-11-20 23:25:43 +0100

    onboarding-page

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -3,11 +3,9 @@ import { useForm } from "react-hook-form";
 import {
         Form,
         FormControl,
-        FormDescription,
         FormField,
         FormItem,
         FormLabel,
-        FormMessage,
       } from "../ui/form";
 import { zodResolver } from '@hookform/resolvers/zod';
 import { UserValidation } from "@/lib/validations/user";
@@ -15,8 +13,10 @@ import { z } from "zod";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import Image from "next/image";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 import { Textarea } from "../ui/textarea";
+import { isBase64Image } from "@/lib/utils";
+import {useUploadThing} from '@/lib/uploadthing'
 
 
 interface Props {
@@ -35,6 +35,8 @@ interface Props {
 
 
 const AccountProfile = ({user, btnTitle }: Props)  => {
+
+      const [files, setFiles ] = useState<File[]>([])
           
     const form = useForm({
         resolver:  zodResolver(UserValidation),
@@ -47,14 +49,34 @@ const AccountProfile = ({user, btnTitle }: Props)  => {
         }
     })
 
-    const handleImage = (e: ChangeEvent, fieldChange: (value:string) => void) => {
+    const handleImage = (e: ChangeEvent<HTMLInputElement>, fieldChange: (value:string) => void) => {
       e.preventDefault();
+
+      const fileReader = new FileReader();
+
+      if(e.target.files && e.target.files.length > 0 ) {
+        const file = e.target.files[0];
+
+        setFiles(Array.from(e.target.files));
+
+        if(!file.type.includes('image')) return;
+
+        fileReader.onload = async (event) => {
+          const imageDataUrl = event.target?.result?.toString() || '';
+
+          fieldChange(imageDataUrl)
+        }
+
+        fileReader.readAsDataURL(file);
+
+      }
     }
 
     function onSubmit(values: z.infer<typeof UserValidation>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
+      const blob = values.profile_photo;
+      const hasImageChanged = isBase64Image(blob);
+
+
       }
 
     return (
@@ -98,8 +120,6 @@ const AccountProfile = ({user, btnTitle }: Props)  => {
                     onChange={(e) => handleImage(e, field.onChange)}
                     />
                 </FormControl>
-      
-                <FormMessage />
               </FormItem>
             )}
           />
@@ -121,7 +141,6 @@ const AccountProfile = ({user, btnTitle }: Props)  => {
                     />
                 </FormControl>
       
-                <FormMessage />
               </FormItem>
             )}
           />
@@ -143,7 +162,7 @@ const AccountProfile = ({user, btnTitle }: Props)  => {
                     />
                 </FormControl>
       
-                <FormMessage />
+           
               </FormItem>
             )}
           />
@@ -165,20 +184,14 @@ const AccountProfile = ({user, btnTitle }: Props)  => {
                     />
                 </FormControl>
       
-                <FormMessage />
+           
               </FormItem>
             )}
-          />
-
-
-
-
-
-
+          /> 
           
           <Button type="submit" className="bg-primary-500">Submit</Button>
         </form>
       </Form>
     )
 };
-export default AccountProfile;
+export default AccountProfile; 
