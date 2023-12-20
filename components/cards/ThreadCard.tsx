381ac200172b1064bef7ee24ff03107816c61bdc
brewz45 <michaelsadewale@protonmail.com>
2023-12-20 15:35:30 +0100

    thread card

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 interface Props {
     id: string;
     currentUserId: string;
@@ -33,7 +36,27 @@ createdAt,
 comments,
 }: Props) => {
     return (
-        <article>
+        <article className="flex w-full flex-col rounded-xl bg-dark-2 p-7">
+            <div className="flex items-start justify-between">
+            <div className="flex w-full flex-1 flex-row gap-4">
+                <div className="flex flex-col items-center">
+                    <Link href={`/profile/${author.id}`} className="relative h-11 w-11">
+                    <Image
+                        src={author.image}
+                        alt="Profile image"
+                        fill
+                        className="cursor-pointer rounded-full"
+                    />
+
+                    </Link>
+                    <div className="thread-card_bar"/>
+                </div>
+                <div>
+                    <Link href={`/profile/${author.id}`} className="relative h-11 w-11">
+                    </Link>
+                </div>
+            </div>
+            </div>
             <h2 className="text-small-regular text-light-2">
                 {content}
             </h2>
